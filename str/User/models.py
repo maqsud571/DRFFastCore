ae892849ce9e03be58579c712d214eb135f849cf
maqsud571 <uzbpubgmm24@gmail.com>
2026-01-28 00:10:14 +0500

    Api + postgres

--- str/User/models.py
+++ str/User/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(max_length=3)
+
+    def __str__(self):
+        return self.name
--- str/User/views.py
+++ str/User/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from drf_yasg.utils import APIView
+from drf_yasg.utils import swagger_auto_schema
+from .models import Item
+from .serializers import User_srl
 
 # Create your views here.
+class Register(APIView):
+    serializers = User_srl
+    @swagger_auto_schema(request_body=User_srl)
+    def post(self,request):
+        serializers = User_srl(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response({"MSG":"Succes"})
+        else:
+            return Response (serializers.errors)
